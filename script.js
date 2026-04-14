87dda536cb09ae0d8428c2ad851cc2023e4d9592
abhishekydv7 <abhibuddy224@gmail.com>
2026-04-15 00:13:57 +0530

    search button and error handling

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const API_KEY = "aa5e360422e945a8a9e592631f3b2691";
 
-function fetchNews(category = "general") {
-  const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
+function fetchNews(category = "general", query = "") {
+
+  let url = "";
+  if (query) {
+    url = `https://newsapi.org/v2/everything?q=${query}&language=en&sortBy=publishedAt&pageSize=10&apiKey=${API_KEY}`;
+  } else {
+    url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
+  }
+
+  const loader = document.getElementById("loader");
+  loader.style.display = "block";
+
   fetch(url)
-    .then(response => response.json())
+    //CATCH API ERRORS
+    .then(response =>{
+      if(!response.ok) {
+        throw new Error("API Error");
+      }
+      return response.json();
+    })
     .then(data => {
+      loader.style.display = "none";
       const container = document.getElementById("news-container"); 
-  container.innerHTML = ""; //remove prev inputs
+      container.innerHTML = ""; //remove prev inputs
+
+      //HANDLES NO RESULT
+      if(!data.articles || data.articles.length === 0) {
+        container.innerHTML = "<h3>No results found!!</h3>";
+        return;
+      }
+
     data.articles.slice(0,6).forEach(article => {
 
-      // create column
+      // DIVISION OF CODE
       const col = document.createElement("div");
       col.className = "col-md-4 mb-4";
 
-      //create card using innerHTML
+      // CARDS FORCED CODE
       col.innerHTML = `
             <div class="card h-100">
-               <img src="${article.urlToImage}" class="card-img-top" alt = "news image">
+               <img src="${article.urlToImage || "https://via.placeholder.com/300"}" class="card-img-top" alt = "news image">
                 <div class ="card-body">
                         <h5 class = "card-title">${article.title}</h5>
-                        <p class="card-text">${article.description?.slice(0,100) || "No description available"}... </p>
+                        <p class="card-text">${article.description?.slice(0, 100) || "No description available"}... </p>
                         <a href= "${article.url}" target="_blank" class="btn btn-primary">
                           Read More...
                         </a>
@@ -29,8 +53,16 @@ function fetchNews(category = "general") {
 
       container.appendChild(col);
     });
-  
   })
+  // TO CATCH ANY ERROR
+  .catch(error => {   
+    loader.style.display = "none";
+
+    const container = document.getElementById("news-container");
+    container.innerHTML = "<h3>Something went wrong 🚨</h3>";
+
+    console.error(error);
+  });
 }
 
 const buttons = document.querySelectorAll(".category-btn");
@@ -43,3 +75,20 @@ buttons.forEach(button=>{
 });
 
 fetchNews();
+
+const searchBtn = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+
+searchBtn.addEventListener("click", ()=> {
+  const query = searchInput.value.trim();
+
+  if(!query) return;
+
+  fetchNews("", query);
+});
+
+searchInput.addEventListener("keypress", (e) => {
+  if(e.key === "Enter") {
+    searchBtn.click();
+  }
+});
